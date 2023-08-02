b88e0b635b025b10901abf336fbe9f1769df9bd3
yoonjoowon <wndnjs6224@naver.com>
2023-08-02 21:34:50 +0900

    fix

--- ai/Server.js
+++ ai/Server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-
 const app = express();
 const api_key = process.env.REACT_APP_LOL_API_KEY;
 const url = "https://kr.api.riotgames.com";
@@ -31,4 +30,24 @@ app.get("/lol/summoner/:summonerName", async (req, res) => {
 const port = 3001; // 또는 다른 포트 번호로 변경 가능
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.get("/lol/summoner/:summonerName", async (req, res) => {
+  const summonerName = req.params.summonerName;
+  const request_header = {
+    "X-Riot-Token": api_key,
+  };
+
+  try {
+    const response = await axios.get(
+      `${url}/lol/summoner/v4/summoners/by-name/${summonerName}`,
+      { headers: request_header }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(error.response?.status || 500)
+      .json({ error: "An error occurred" });
+  }
 });
