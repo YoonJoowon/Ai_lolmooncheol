686d46b559820430c64034f81bf814578520d295
yoonjoowon <wndnjs6224@naver.com>
2023-08-08 17:44:09 +0900

    fix : 기본 firebase 새로 세팅 완료

--- ai/Firebase.js
+++ ai/Firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "dor-project-12340.firebaseapp.com",
-  projectId: "dor-project-12340",
-  storageBucket: "dor-project-12340.appspot.com",
-  messagingSenderId: "426903209241",
-  appId: "1:426903209241:web:2626860b249ae1f5213188",
-  measurementId: "G-8MGR4M93CP",
+  authDomain: "aimoon-c9fa4.firebaseapp.com",
+  projectId: "aimoon-c9fa4",
+  storageBucket: "aimoon-c9fa4.appspot.com",
+  messagingSenderId: "928734093079",
+  appId: "1:928734093079:web:d9e3c6d2d41f26298f2152",
+  measurementId: "G-W565SFZ6GF",
 };
 
 // Initialize Firebase
