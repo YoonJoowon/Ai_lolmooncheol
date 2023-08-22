969801cf80381d17119eb3c3acab6c2fb55c2751
dasong <sonaki970811@gmail.com>
2023-08-22 14:55:58 +0900

    Feat : 롤 api 연결
    node로 리액트와 연결 시도중 폴더 분리

--- server/server.js
+++ server/server.js
@@ -1,32 +1,37 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const API_KEY = process.env.REACT_APP_LOL_API_KEY;
-const baseUrl = 'https://KR.api.riotgames.com/lol';
-const baseUrl2 = 'https://asia.api.riotgames.com/lol';
+const baseUrl = "https://KR.api.riotgames.com/lol";
+const baseUrl2 = "https://asia.api.riotgames.com/lol";
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.listen(8080, function () {
-  console.log('listening on 8080');
+  console.log("listening on 8080");
 });
 
 async function getMatchDetails(matchId, puuid) {
   try {
-    const matchResponse = await axios.get(`${baseUrl2}/match/v5/matches/${matchId}`, {
-      headers: {
-        'X-Riot-Token': API_KEY,
-      },
-    });
+    const matchResponse = await axios.get(
+      `${baseUrl2}/match/v5/matches/${matchId}`,
+      {
+        headers: {
+          "X-Riot-Token": API_KEY,
+        },
+      }
+    );
 
     const matchData = matchResponse.data;
 
     // Find the participant with the given puuid
-    const participant = matchData.info.participants.find(participant => participant.puuid === puuid);
+    const participant = matchData.info.participants.find(
+      (participant) => participant.puuid === puuid
+    );
 
     if (!participant) {
       return null; // Participant not found, handle this case
@@ -46,10 +51,12 @@ async function getMatchDetails(matchId, puuid) {
 
     // Get team members of the user's team
     const teamId = participant.teamId;
-    const teamMembers = matchData.info.participants.filter(participant => participant.teamId === teamId);
+    const teamMembers = matchData.info.participants.filter(
+      (participant) => participant.teamId === teamId
+    );
 
     // Extract information for team members
-    const teamMemberInfo = teamMembers.map(teamMember => ({
+    const teamMemberInfo = teamMembers.map((teamMember) => ({
       championName: teamMember.championName,
       championImageUrl: `http://ddragon.leagueoflegends.com/cdn/13.15.1/img/champion/${teamMember.championName}.png`,
       kills: teamMember.kills,
@@ -58,47 +65,64 @@ async function getMatchDetails(matchId, puuid) {
       lane: teamMember.lane,
     }));
 
-    return { championName, championImageUrl, kills, deaths, assists, win, lane, teamMembers: teamMemberInfo };
+    return {
+      championName,
+      championImageUrl,
+      kills,
+      deaths,
+      assists,
+      win,
+      lane,
+      teamMembers: teamMemberInfo,
+    };
   } catch (error) {
     console.error(error);
     return null;
   }
 }
 
-app.post('/summoner', async function (req, res) {
+app.post("/summoner", async function (req, res) {
   const summonerName = req.body.name;
 
   try {
     // Get summoner information
-    const summonerResponse = await axios.get(`${baseUrl}/summoner/v4/summoners/by-name/${summonerName}`, {
-      headers: {
-        'X-Riot-Token': API_KEY,
-      },
-    });
+    const summonerResponse = await axios.get(
+      `${baseUrl}/summoner/v4/summoners/by-name/${summonerName}`,
+      {
+        headers: {
+          "X-Riot-Token": API_KEY,
+        },
+      }
+    );
 
     const puuid = summonerResponse.data.puuid;
 
     try {
       // Get the last 4 match IDs
-      const matchResponse = await axios.get(`${baseUrl2}/match/v5/matches/by-puuid/${puuid}/ids`, {
-        headers: {
-          'X-Riot-Token': API_KEY,
-        },
-      });
+      const matchResponse = await axios.get(
+        `${baseUrl2}/match/v5/matches/by-puuid/${puuid}/ids`,
+        {
+          headers: {
+            "X-Riot-Token": API_KEY,
+          },
+        }
+      );
 
       // Last 4 matches
       const matchIds = matchResponse.data.slice(0, 4);
 
       // Retrieve details for each match
-      const matchDetails = await Promise.all(matchIds.map(matchId => getMatchDetails(matchId, puuid)));
+      const matchDetails = await Promise.all(
+        matchIds.map((matchId) => getMatchDetails(matchId, puuid))
+      );
 
       res.json({ matchDetails });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'An error occurred' });
+      res.status(500).json({ error: "An error occurred" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({ error: "An error occurred" });
   }
 });
